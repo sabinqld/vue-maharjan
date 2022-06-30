24e61089a3eabfe3275c95863870bae1ac2e5cd5
Sabin Maharjan <sabin.maharjan@health.qld.gov.au>
2022-06-30 15:11:13 +1000

    added vue editor

--- src/views/skills/Edit.vue
+++ src/views/skills/Edit.vue
@@ -6,31 +6,22 @@
           <v-form ref="form" v-model="valid">
             <v-text-field v-model="name" label="Title" required></v-text-field>
 
-            <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
-
-            <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate"> Validate </v-btn>
-
-            <v-btn color="error" class="mr-4" @click="reset"> Reset Form </v-btn>
-
-            <v-btn color="warning" @click="resetValidation"> Reset Validation </v-btn>
-
             <v-text-field label="Percentage" v-model="percentage"></v-text-field>
 
             <vue-editor :editorToolbar="customToolbar" v-model="description"></vue-editor>
             <v-text-field label="Level" v-model="level"></v-text-field>
             <v-text-field label="Experience" v-model="experience"></v-text-field>
+            <v-toolbar flat>
+              <v-btn v-show="!dataDirty" ripple rounded color="secondary" @click="cancel">Close</v-btn>
+              <v-btn v-show="!dataDirty" ripple rounded @click="cancel">Cancel</v-btn>
+              <v-btn v-show="!dataDirty" ripple rounded color="primary" @click="save()">Save</v-btn>
+            </v-toolbar>
           </v-form>
         </template>
       </v-row>
     </v-container>
-    <v-toolbar flat>
-      <v-btn v-show="!dataDirty" ripple rounded color="accent" @click="deleteCourse">Delete</v-btn>
-      <v-spacer></v-spacer>
-      <v-btn v-show="!dataDirty" ripple rounded color="secondary" @click="cancel">Close</v-btn>
-      <v-btn v-show="dataDirty" ripple rounded @click="cancel">Cancel</v-btn>
-      <v-btn v-show="dataDirty" ripple rounded color="primary" @click="save()">Save</v-btn>
-    </v-toolbar>
-    <v-menu offset-y>
+
+    <!-- <v-menu offset-y>
       <template v-slot:activator="{ on }">
         <v-btn v-show="itemDirty" ripple rounded color="primary" dark v-on="on">Save</v-btn>
       </template>
@@ -39,20 +30,26 @@
           <v-list-item-title>{{ item.title }}</v-list-item-title>
         </v-list-item>
       </v-list>
-    </v-menu>
+    </v-menu> -->
   </v-card>
 </template>
 <script>
 import { mapState } from "vuex";
+import { VueEditor } from "vue2-editor";
 export default {
   name: "edit-skill",
+  components: {
+    VueEditor
+  },
   props: ["id"],
   data: () => ({
     search: "",
     panels: [2, 4],
     customToolbar: [
       ["bold", "italic", "underline"],
-      [{ list: "ordered" }, { list: "bullet" }]
+      [{ list: "ordered" }, { list: "bullet" }, { list: "check" }][
+        ({ align: "" }, { align: "center" }, { align: "right" }, { align: "justify" })
+      ]
     ],
     saveButton: [
       { click: "save", title: "Save to Draft" },
